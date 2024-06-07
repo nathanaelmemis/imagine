965fc90f48524be0ced9629004fe5b6ffed00530
Nathanael Memis <nathanaelmemis@gmail.com>
2024-06-07 17:29:57 +0800

    proxying test

--- api/server.js
+++ api/server.js
@@ -7,17 +7,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/labels", (req, res) => {
     const labels = require('./labels.json').labels;
-    console.log(labels);
     res.json(labels);
-    // fs.readFile('labels.txt', 'utf8', (err: Error, data: string) => {
-    //     if (err) {
-    //         console.log(err)
-    //         res.status(500).send(`File could not be read!, ${err}`);
-    //         return;
-    //     }
-    //     const labels = data.split('\r\n');
-    //     // console.log(labels)
-    //     res.json(labels)
-    // })
 });
 app.listen(3000, () => console.log("Server ready on port 3000."));
--- api/server.ts
+++ api/server.ts
@@ -8,27 +8,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
 
 app.get("/labels", (req: any, res: any) => {
-
     const labels = require('./labels.json').labels
-
-    console.log(labels)
-
     res.json(labels)
-
-
-    // fs.readFile('labels.txt', 'utf8', (err: Error, data: string) => {
-    //     if (err) {
-    //         console.log(err)
-    //         res.status(500).send(`File could not be read!, ${err}`);
-    //         return;
-    //     }
-
-    //     const labels = data.split('\r\n');
-
-
-    //     // console.log(labels)
-    //     res.json(labels)
-    // })
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
