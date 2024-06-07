9a70f5e5c66ab7046a7f3f11a0f221fa329fabb4
Nathanael Memis <nathanaelmemis@gmail.com>
2024-06-07 12:31:34 +0800

    deploy test

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: any, res: any) => {
   res.send('Express + TypeScript Server');
