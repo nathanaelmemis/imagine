9a2a3b91d49f5c73d86a5889e40b0e35ce030aaa
Nathanael Memis <nathanaelmemis@gmail.com>
2024-06-07 15:57:53 +0800

    deploy test

---
+++ api/dist/server.js
@@ -0,0 +1,8 @@
+"use strict";
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => res.send("Express on Vercel"));
+app.listen(3000, () => console.log("Server ready on port 3000."));
