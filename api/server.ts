f3b3f0e04ea97680b371d0b4143eda6903b3a31f
Nathanael Memis <nathanaelmemis@gmail.com>
2024-06-07 16:42:39 +0800

    proxying test

--- api/server.ts
+++ api/server.ts
@@ -11,12 +11,14 @@ app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
 app.get("/labels", (req: any, res: any) => {
     fs.readFile('labels.txt', 'utf8', (err: Error, data: string) => {
         if (err) {
+            console.log(err)
             res.status(500).send(`File could not be read!, ${err}`);
             return;
         }
 
         const labels = data.split('\n');
 
+        console.log(labels)
         res.json(labels)
     })
 });
