64e041e374a2652020d38095f67d7f8e28fc27ba
Nathanael Memis <nathanaelmemis@gmail.com>
2024-06-07 16:45:47 +0800

    proxying test

--- api/server.js
+++ api/server.js
@@ -7,22 +7,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/labels", (req, res) => {
-    // // Function to read and parse the file
-    // function parseTextFile(filePath) {
-    //     fs.readFile(filePath, 'utf8', (err, data) => {
-    //         if (err) {
-    //             console.error("File could not be read!", err);
-    //             return;
-    //         }
-    //         // Split the content by newlines
-    //         const lines = data.split('\n');
-    //         // Output the array of lines
-    //         console.log(lines);
-    //     });
-    // }
-    // // Example usage:
-    // // Replace 'path/to/your/file.txt' with the actual file path
-    // const filePath = 'path/to/your/file.txt';
-    // parseTextFile(filePath);
+    fs.readFile('labels.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send(`File could not be read!, ${err}`);
+            return;
+        }
+        const labels = data.split('\n');
+        console.log(labels);
+        res.json(labels);
+    });
 });
 app.listen(3000, () => console.log("Server ready on port 3000."));
