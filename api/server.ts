c179ec30b26d7182f86579d293459de5c842bb0c
Nathanael Memis <nathanaelmemis@gmail.com>
2024-06-07 12:48:54 +0800

    deploy test

--- api/server.ts
+++ api/server.ts
@@ -8,5 +8,3 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
-
-export default app;
