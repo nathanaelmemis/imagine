c0ecb5999d766ab57140fc016fcaa7d4b1145f9c
Nathanael Memis <nathanaelmemis@gmail.com>
2024-06-07 17:11:44 +0800

    proxying test

--- api/server.js
+++ api/server.js
@@ -1,21 +1,30 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 const bodyParser = require('body-parser');
 const express = require('express');
-const fs = require('fs');
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/labels", (req, res) => {
-    fs.readFile('labels.txt', 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send(`File could not be read!, ${err}`);
-            return;
-        }
-        const labels = data.split('\r\n');
-        // console.log(labels)
-        res.json(labels);
-    });
+    let usersPath = path_1.default.join(process.cwd(), 'labels.txt');
+    let file = fs_1.default.readFileSync('labels.txt');
+    console.log(file.toString().split('\r\n'));
+    res.json(file.toString().split('\r\n'));
+    // fs.readFile('labels.txt', 'utf8', (err: Error, data: string) => {
+    //     if (err) {
+    //         console.log(err)
+    //         res.status(500).send(`File could not be read!, ${err}`);
+    //         return;
+    //     }
+    //     const labels = data.split('\r\n');
+    //     // console.log(labels)
+    //     res.json(labels)
+    // })
 });
 app.listen(3000, () => console.log("Server ready on port 3000."));
--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const fs = require('fs');
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 app.use(bodyParser.json()); 
@@ -9,19 +10,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
 
 app.get("/labels", (req: any, res: any) => {
-    fs.readFile('labels.txt', 'utf8', (err: Error, data: string) => {
-        if (err) {
-            console.log(err)
-            res.status(500).send(`File could not be read!, ${err}`);
-            return;
-        }
 
-        const labels = data.split('\r\n');
+    let usersPath = path.join(process.cwd(), 'labels.txt');
+    let file = fs.readFileSync('labels.txt');
+
+    console.log(file.toString().split('\r\n'))
+
+    res.json(file.toString().split('\r\n'))
 
 
-        // console.log(labels)
-        res.json(labels)
-    })
+    // fs.readFile('labels.txt', 'utf8', (err: Error, data: string) => {
+    //     if (err) {
+    //         console.log(err)
+    //         res.status(500).send(`File could not be read!, ${err}`);
+    //         return;
+    //     }
+
+    //     const labels = data.split('\r\n');
+
+
+    //     // console.log(labels)
+    //     res.json(labels)
+    // })
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
